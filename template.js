02dd8a949330dfcaf2d136384eda163a76c8e71a
NaNnin08 <ocotsunandar@gmail.com>
2021-04-30 18:48:30 +0700

    change logo

--- template.js
+++ template.js
@@ -1,13 +1,13 @@
 export default () => {
-    return `<!doctype html>
+  return `<!doctype html>
       <html lang="en">
         <head>
           <meta charset="utf-8">
-          <title>HRFullstack</title>
+          <title>Project Assignment</title>
         </head>
         <body>
           <div id="root"></div>
           <script type="text/javascript" src="/dist/bundle.js"></script>
         </body>
-      </html>`
-  }
+      </html>`;
+};
